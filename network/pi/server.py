a3a4f014fc9f5e7dbdc68b98a34cd513804fa4cc
Steven Appler <44624294+sappler@users.noreply.github.com>
2021-05-04 17:09:39 -0700

    made server eternal

--- network/pi/server.py
+++ network/pi/server.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
-"""PyBluez simple example rfcomm-server.py
-Simple demonstration of a server application that uses RFCOMM sockets.
-Author: Albert Huang <albert@csail.mit.edu>
-$Id: rfcomm-server.py 518 2007-08-10 07:20:07Z albert $
-"""
 
 import bluetooth
 import subprocess
 import sys
 import time
+import io
 try:
         subprocess.call(['sudo', 'hciconfig', 'hci0', 'piscan'])
 except:
@@ -21,7 +17,7 @@ server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
 
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+uuid = "8ce255c0-200a-11e0-ac64-0800200c9a66"
 
 bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
                             service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
@@ -34,41 +30,47 @@ print("Waiting for connection on RFCOMM channel", port)
 client_sock, client_info = server_sock.accept()
 print("Accepted connection from", client_info)
 
-try:
-    while True:
-        data = client_sock.recv(1024)
-        if not data:
-            break
-        code=str(data)[2]
-        print(data)
-        if code=='1': # change settings
-            print('changed settings')
-            client_sock.send(str.encode('1'))
-        elif code=='2': # requested file
-            try:
-                with open("New","rb") as f: #add file called new to your directory to transfer it
-                    print('opened file')
-                    client_sock.send(str.encode(code))
-                    print('sent opcode for sending file')
-                    bytes=1
-                    while bytes:
-                        time.sleep(0.5)
-                        #prevent sending data too quickly. Android can't handle it for some reason
-                        #this just makes sure for every 1000 bytes sent, we get a response at least once
-                        data = client_sock.recv(1024)
-                        print('phone received data')
-                        bytes=f.read(1000)
-                        print(len(bytes))
-                        client_sock.send(bytes)
-                        print('sent')
-                    print('finished sending')
-                    client_sock.send(str.encode('\x04'))
-            except OSError as err:
-                    print("OS error: {0}".format(err))
-                    client_sock.send(str.encode('3')) 
-            #client_sock.send(str.encode(temp))
-except OSError as err:
-    print("OS error: {0}".format(err))
+while True:
+    try:
+        while True:
+            data = client_sock.recv(1024)
+            if not data:
+                break
+            code=str(data)[2]
+            #print(data)
+            if code=='1': # change settings
+                with open('config.txt','r') as f:
+                    settings=f.read()
+                with open('config.txt','w') as f:
+                    f.write(str(data)[3:])
+                    
+                print('changed settings to: ' + str(data)[4:-1])
+                client_sock.send(str.encode('1'))
+            elif code=='2': # requested file
+                try:
+                    with open("New","rb") as f: #add file called new to your directory to transfer it
+                        print('opened file')
+                        client_sock.send(str.encode(code))
+                        print('sent opcode for sending file')
+                        bytes=1
+                        while bytes:
+                            time.sleep(0.5)
+                            #prevent sending data too quickly. Android can't handle it for some reason
+                            #this just makes sure for every 1000 bytes sent, we get a response at least once
+                            data = client_sock.recv(1024)
+                            print('phone received data')
+                            bytes=f.read(1000)
+                            print(len(bytes))
+                            client_sock.send(bytes)
+                            print('sent')
+                        print('finished sending')
+                        client_sock.send(str.encode('\x04'))
+                except OSError as err:
+                        print("OS error: {0}".format(err))
+                        client_sock.send(str.encode('3')) 
+                #client_sock.send(str.encode(temp))
+    except OSError as err:
+        print("OS error: {0}".format(err))
 
 
 
