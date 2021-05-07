cf56c708df5b74d5d0d98dcef680e568334c890c
jemcclel <jemcclel.ucsc.edu>
2021-05-07 09:28:00 -0700

    Added accel collection script

---
+++ AccelData/accel_readin.py
@@ -0,0 +1,107 @@
+'''
+        Read Gyro and Accelerometer by Interfacing Raspberry Pi with MPU6050 using Python
+    http://www.electronicwings.com
+    edited by Jacob McClellan for Dream Team 9
+'''
+import smbus			#import SMBus module of I2C
+import time
+from time import sleep          #import
+
+#some MPU6050 Registers and their Address
+PWR_MGMT_1   = 0x6B
+SMPLRT_DIV   = 0x19
+CONFIG       = 0x1A
+GYRO_CONFIG  = 0x1B
+INT_ENABLE   = 0x38
+ACCEL_XOUT_H = 0x3B
+ACCEL_YOUT_H = 0x3D
+ACCEL_ZOUT_H = 0x3F
+GYRO_XOUT_H  = 0x43
+GYRO_YOUT_H  = 0x45
+GYRO_ZOUT_H  = 0x47
+
+inc = 15
+
+
+
+
+def MPU_Init():
+    #write to sample rate register
+    bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
+    
+    #Write to power management register
+    bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
+    
+    #Write to Configuration register
+    bus.write_byte_data(Device_Address, CONFIG, 0)
+    
+    #Write to Gyro configuration register
+    bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
+    
+    #Write to interrupt enable register
+    bus.write_byte_data(Device_Address, INT_ENABLE, 1)
+
+def read_raw_data(addr):
+    #Accelero and Gyro value are 16-bit
+        high = bus.read_byte_data(Device_Address, addr)
+        low = bus.read_byte_data(Device_Address, addr+1)
+    
+        #concatenate higher and lower value
+        value = ((high << 8) | low)
+        
+        #to get signed value from mpu6050
+        if(value > 32768):
+                value = value - 65536
+        return value
+
+
+bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
+Device_Address = 0x68   # MPU6050 device address
+
+MPU_Init()
+tim = time.time()
+
+print (" Reading Data of Gyroscope and Accelerometer")
+
+old = 0
+avg = 0
+large = 0
+i = 0
+j = 1
+
+while True:
+    
+    
+    #Read Accelerometer raw value
+    acc_x = read_raw_data(ACCEL_XOUT_H)
+    acc_y = read_raw_data(ACCEL_YOUT_H)
+    acc_z = read_raw_data(ACCEL_ZOUT_H)
+    
+    
+    #Full scale range +/- 250 degree/C as per sensitivity scale factor
+    Ax = acc_x/16384.0
+    Ay = acc_y/16384.0
+    Az = acc_z/16384.0
+    
+    
+    val = abs(10*Ax + 10*Ay + 10*Az - old)
+    avg = avg + val
+    if (val > large):
+        large = val
+    old  = 10*Ax + 10*Ay + 10*Az
+    i = i + 1       
+    
+    if(time.time() >= tim+inc*j):
+        
+        file1 = open("AccelData.txt", "a+")
+        print("%d " %j, "%.1f" % (avg/i))
+        line = str(j*inc) + " " + str(int(large)) + "\n"
+        file1.write(line)
+        i = 0
+        avg = 0
+        j = j + 1
+        file1.close()
+        
+        
+        
+    #print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
